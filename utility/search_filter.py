d9a991f043271e16e58127ccd1b96c1b266356a8
Quartz <quartzkhatri@gmail.com>
2021-12-22 15:35:10 +0530

    pagination in hospital

--- utility/search_filter.py
+++ utility/search_filter.py
@@ -1,20 +1,25 @@
-from decouple import config
+import re
+
 from django.core.paginator import Paginator
 
 
-def pagination(recordset, page):
+def pagination(recordset, page, pageRecord=5):
     warning = None
-    p = Paginator(recordset, int(config('PAGE_LIMIT')))
-    if int(page) <= int(p.num_pages):
-        recordset = p.page(page)
-    elif int(page) > int(p.num_pages):
-        warning = "You have reached to last page"
-        recordset = p.page(p.num_pages)
-    elif int(page) < 1:
+    p = Paginator(recordset, int(pageRecord))
+
+    if int(page) < 1:
         recordset = p.page(1)
         warning = "You have reached to first page"
+    elif int(page) <= int(p.num_pages) and int(page) > 0:
+        recordset = p.page(page)
+    elif int(page) > int(p.num_pages) and int(page) > 0:
+        warning = "You have reached to last page"
+        recordset = p.page(p.num_pages)
     else:
         recordset = p.page(page)
-    return recordset, warning
+    return recordset, warning, p.num_pages
 
 
+def camel_to_snake(variable_name):
+    variable_name = re.sub(r'(?<!^)(?=[A-Z])', '_', variable_name).lower()
+    return variable_name
