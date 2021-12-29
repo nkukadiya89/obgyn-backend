3ed00436d7f0cecbed4563670583a6f18b5c84fa
Quartz <quartzkhatri@gmail.com>
2021-12-29 13:27:13 +0530

    email fix

--- city/urls.py
+++ city/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import CityAPI
 from city import views
+from .views import CityAPI
 
 urlpatterns = [
     path('<int:id>/', CityAPI.as_view()),
     path('get/', CityAPI.as_view()),
     path('create/', CityAPI.as_view()),
     path('delete/', CityAPI.as_view()),
-    path('update/<int:id>/',views.patch,name="update"),
+    path('update/<int:id>/', views.patch, name="update"),
 ]
