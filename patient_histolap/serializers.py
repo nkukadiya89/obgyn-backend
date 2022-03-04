30f9303f49137f0b3f35c708ec044add1bdb9151
Quartz <quartzkhatri@gmail.com>
2022-03-04 17:28:54 +0530

    patient manage fields

--- patient_histolap/serializers.py
+++ patient_histolap/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from patient.models import PatientModel
 from .models import PatientHistolapModel
+from manage_fields.serializers import ManageFieldsSerializers
 
 
 class PatientHistolapSerializers(serializers.ModelSerializer):
@@ -12,7 +13,14 @@ class PatientHistolapSerializers(serializers.ModelSerializer):
             ret["patient_opd_id"] = ret["patient_opd"]
             del ret["patient_opd"]
 
+        for fld_nm in ["procedure_name","right_tube","left_tube"]:
+            fld_name = fld_nm + "_name"
+            search_instance = "instance" + "." + fld_nm
+            if fld_nm in ret:
+                ret[fld_name] = ManageFieldsSerializers(eval(search_instance)).data["field_value"]
+
         return ret
+
     def validate(self, data):
         if "regd_no" in data:
             patient = PatientModel.objects.filter(registered_no=data["regd_no"])
--- patient_mtp/serializers.py
+++ patient_mtp/serializers.py
@@ -2,12 +2,20 @@ from rest_framework import serializers
 
 from patient.models import PatientModel
 from .models import PatientMtpModel
+from manage_fields.serializers import ManageFieldsSerializers
 
 
 class PatientMtpSerializers(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         ret = super(PatientMtpSerializers, self).to_representation(instance)
+
+        for fld_nm in ["reason_for_mtp"]:
+            fld_name = fld_nm + "_name"
+            search_instance = "instance" + "." + fld_nm
+            if fld_nm in ret:
+                ret[fld_name] = ManageFieldsSerializers(eval(search_instance)).data["field_value"]
+
 
         if "patient_opd" in ret:
             ret["patient_opd_id"] = ret["patient_opd"]
