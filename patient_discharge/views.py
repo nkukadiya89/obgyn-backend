fd63c59a9a558d1c07f394a725b0541e49b5572a
Quartz <quartzkhatri@gmail.com>
2022-03-11 17:52:42 +0530

    patient discharge

--- patient_discharge/views.py
+++ patient_discharge/views.py
@@ -82,7 +82,7 @@ class PatientDischargeAPI(APIView):
 
             if serializer.is_valid():
                 serializer.save()
-                patient_opd = PatientOpdModel.objects.filter(pk=request.data["patient_opd_id"].first())
+                patient_opd = PatientOpdModel.objects.filter(pk=request.data["patient_opd_id"]).first()
                 patient_opd.status = True
                 patient_opd.save()
 
