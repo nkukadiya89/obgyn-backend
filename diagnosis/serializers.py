e9ee0a86e68c26999c37fd4a09a09f6e3cd491f3
Quartz <quartzkhatri@gmail.com>
2022-03-16 15:54:13 +0530

    duplicate diagnosis removed

--- diagnosis/serializers.py
+++ diagnosis/serializers.py
@@ -19,20 +19,6 @@ class DiagnosisSerializers(serializers.ModelSerializer):
 
         return ret
 
-    def validate(self, data):
-        diagnosis_name = data.get("diagnosis_name")
-        duplicate_diagnosis = DiagnosisModel.objects.filter(deleted=0, diagnosis_name__iexact=diagnosis_name)
-
-        if self.partial:
-            duplicate_diagnosis = duplicate_diagnosis.filter(~Q(pk=self.instance.diagnosis_id)).first()
-        else:
-            duplicate_diagnosis = duplicate_diagnosis.first()
-
-        if duplicate_diagnosis != None:
-            raise serializers.ValidationError("Diagnosis already exist.")
-
-        return data
-
     diagnosis_id = serializers.IntegerField(read_only=True)
     # medicine = MedicineSerializers(many=True)
     class Meta:
