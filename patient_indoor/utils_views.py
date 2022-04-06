04a324e0aefe1bd33b3e5ff893d1de03a235df6e
Quartz <quartzkhatri@gmail.com>
2022-04-06 14:29:43 +0530

    indoor and advice merge

---
+++ patient_indoor/utils_views.py
@@ -0,0 +1,13 @@
+from .models import IndoorAdviceModel
+from .serializers import IndoorAdviceSerializers
+
+
+def indoor_advice_insert(advice_json,patient_indoor_id, created_by):
+    for advice in advice_json:
+        advice["patient_indoor"] = patient_indoor_id
+        advice["created_by"] = created_by
+        indoor_advice = IndoorAdviceModel()
+        serializer = IndoorAdviceSerializers(indoor_advice, data=advice)
+
+        if serializer.is_valid():
+            serializer.save()
--- template_header/models.py
+++ template_header/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-from language.models import LanguageModel
 from django.utils.timezone import now
+
+from language.models import LanguageModel
+from user.models import User
+
 
 # Create your models here.
 class TemplateHeaderModel(models.Model):
@@ -8,6 +11,7 @@ class TemplateHeaderModel(models.Model):
     template_header_name = models.CharField(max_length=35, default="", null=True)
     language = models.ForeignKey(LanguageModel, on_delete=models.DO_NOTHING)
     header_text = models.TextField(null=True)
+    hospital = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
 
     created_by = models.IntegerField(default=1, unique=False)
     deleted = models.IntegerField(default=0, unique=False)
