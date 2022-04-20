e0a68e9c523e4837c53f34736805eaa7a2d850f5
Quartz <akhatri@devopsinternational.nl>
2022-04-20 16:41:14 +0530

    voucher
    medicine detail in voucher response

--- surgical_item/serializers.py
+++ surgical_item/serializers.py
@@ -8,10 +8,12 @@ class SurgicalItemSerializers(serializers.ModelSerializer):
 
     def validate(self, data):
         drug_name = data.get("drug_name")
-        duplicate_drug = SurgicalItemModel.objects.filter(deleted=0, drug_name__iexact=drug_name)
+        duplicate_drug = SurgicalItemModel.objects.filter(
+            deleted=0, drug_name__iexact=drug_name)
 
         if self.partial:
-            duplicate_drug = duplicate_drug.filter(~Q(pk=self.instance.surgical_item_id)).first()
+            duplicate_drug = duplicate_drug.filter(
+                ~Q(pk=self.instance.surgical_item_id)).first()
         else:
             duplicate_drug = duplicate_drug.first()
 
@@ -30,28 +32,31 @@ class SurgicalItemSerializers(serializers.ModelSerializer):
 
 class SurgicalItemGroupSerializers(serializers.ModelSerializer):
     def to_representation(self, instance):
-        ret = super(SurgicalItemGroupSerializers, self).to_representation(instance)
+        ret = super(SurgicalItemGroupSerializers,
+                    self).to_representation(instance)
 
         surgical_item_name_list = {}
         for surgical_item1 in ret["surgical_item"]:
             surgical_item = SurgicalItemModel.objects.get(pk=surgical_item1)
             surgical_item_name_list[surgical_item.surgical_item_id] = surgical_item.drug_name
 
-
-        # item_list = SurgicalItemModel.objects.get(surgical_item_groupmodel__si_group_id=instance.si_group_id).values_list('surgical_item_id',flat=True)
-        # print(item_list)
-
-        ret['surgical_item_name'] = surgical_item_name_list
+        item_list = SurgicalItemModel.objects.filter(
+            surgicalitemgroupmodel__si_group_id=instance.si_group_id)
+        
+        ret["surgical_item"] = SurgicalItemSerializers(item_list,many=True).data
+        # ret['surgical_item_name'] = surgical_item_name_list
         # ret.pop("surgical_item")
 
         return ret
 
     def validate(self, data):
         drug_group_name = data.get("drug_group_name")
-        duplicate_drug = SurgicalItemGroupModel.objects.filter(deleted=0, drug_group_name__iexact=drug_group_name)
+        duplicate_drug = SurgicalItemGroupModel.objects.filter(
+            deleted=0, drug_group_name__iexact=drug_group_name)
 
         if self.partial:
-            duplicate_drug = duplicate_drug.filter(~Q(pk=self.instance.si_group_id)).first()
+            duplicate_drug = duplicate_drug.filter(
+                ~Q(pk=self.instance.si_group_id)).first()
         else:
             duplicate_drug = duplicate_drug.first()
 
@@ -64,4 +69,5 @@ class SurgicalItemGroupSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = SurgicalItemGroupModel
-        fields = ['si_group_id', 'drug_group_name', 'surgical_item', 'created_by', 'deleted']
+        fields = ['si_group_id', 'drug_group_name',
+                  'surgical_item', 'created_by', 'deleted']
