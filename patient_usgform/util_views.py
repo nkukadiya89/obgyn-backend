cfbaa7cfa303da755673c14e7d5610eb4e90aaa5
Quartz <akhatri@devopsinternational.nl>
2022-04-26 11:58:49 +0530

    usg form
    update patch

--- patient_usgform/util_views.py
+++ patient_usgform/util_views.py
@@ -6,6 +6,9 @@ def insert_child_usgform(request, patient_usgform_id):
     if "usg_child" in request.data:
         child_list = request.data.get('usg_child')
 
+        if len(child_list)>0:
+            USGFormChildModel.objects.filter(patient_usgform_id=patient_usgform_id).delete()
+
         child_dict = {}
         for child in child_list:
             child_dict["child_gender"] = child["child_gender"]
