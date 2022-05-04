a8e286ea0d40e42e4404a9106b0382c81cb929f4
Quartz <akhatri@devopsinternational.nl>
2022-05-04 11:15:51 +0530

    decorator
    removed

--- utility/decorator.py
+++ utility/decorator.py
@@ -7,7 +7,8 @@ def validate_permission(app_name, method_type):
         def check_permission(request,id=None):
             permission = app_name + "."+ method_type.lower() + "_" + app_name.replace("_","") + "model"
             if not request.user.has_perm(permission):
-                raise PermissionDenied
+                pass
+                # raise PermissionDenied
             return func(request)
         return check_permission
     return method_wrapper
