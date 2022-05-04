a447748035a992793ce86435ce47a1f646f074e7
Quartz <akhatri@devopsinternational.nl>
2022-05-04 13:04:04 +0530

    billing
    allow null in room type

--- patient_billing/views.py
+++ patient_billing/views.py
@@ -172,7 +172,7 @@ def patch(request, id):
 
 @api_view(["GET"])
 @authentication_classes([JWTAuthentication])
-@validate_permission("patient_billing", "view")
+@validate_permission_id("patient_billing", "view")
 # ================= Retrieve Single or Multiple records=========================
 def get(request, id=None):
     query_string = request.query_params
