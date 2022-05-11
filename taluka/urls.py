a7753f378b84eb6c7c57128812a14c379b7a2e11
Quartz <akhatri@devopsinternational.nl>
2022-05-11 13:48:05 +0530

    taluka
    create and delete api

--- taluka/urls.py
+++ taluka/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import TalukaAPI, patch, get
+from .views import TalukaAPI, patch, get, create, delete
 
 urlpatterns = [
     path('<int:id>/', TalukaAPI.as_view()),
     path('get/', get),
     path('get/<int:id>', get),
-    path('create/', TalukaAPI.as_view()),
-    path('delete/', TalukaAPI.as_view()),
+    path('create/', create),
+    path('delete/', delete),
     path('update/<int:id>/', patch, name="update"),
 ]
