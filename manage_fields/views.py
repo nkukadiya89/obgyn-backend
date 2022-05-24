a522eb475935b12057904776f598f7a820edb314
Quartz <akhatri@devopsinternational.nl>
2022-05-24 18:29:48 +0530

    removed code

--- manage_fields/views.py
+++ manage_fields/views.py
@@ -283,7 +283,6 @@ def patch_mfm(request, id):
         return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
 
 
-from utility.auto_code import generate_slug
 
 @api_view(["GET"])
 @authentication_classes([JWTAuthentication])
@@ -318,5 +317,4 @@ def get_mfm(request, id=None):
         data["success"] = True
         data["msg"] = "OK"
         data["data"] = serilizer.data
-        generate_slug()
         return Response(data=data, status=status.HTTP_200_OK)
