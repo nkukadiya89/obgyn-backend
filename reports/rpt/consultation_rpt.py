d1d7d10b2d80e75983eb400554347171e6ec4f5c
Quartz <akhatri@devopsinternational.nl>
2022-05-25 19:51:03 +0530

    barcode

--- reports/rpt/consultation_rpt.py
+++ reports/rpt/consultation_rpt.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from django.shortcuts import render, HttpResponse
 from rest_framework import status
 from patient_opd.models import PatientOpdModel
@@ -5,6 +6,8 @@ from template_header.models import TemplateHeaderModel
 from consultation.models import ConsultationModel
 from language.models import LanguageModel
 from django.views.decorators.csrf import csrf_exempt
+from utility.aws_file_upload import upload_barcode_image
+
 
 
 @csrf_exempt
@@ -39,10 +42,15 @@ def consultation_rpt(request, id, language_id=None):
         
     context = {}
     context["receipt_date"] = str(patient_opd.opd_date)
-    context["regd_no"] = patient_opd.patient.registered_no
+    context["regd_no"] = patient_opd.patient.regd_no_barcode
+
+
+
+    
+
     context["name"] = "".join(
         [patient_opd.patient.first_name if patient_opd.patient.first_name else "", " ", patient_opd.patient.middle_name if patient_opd.patient.middle_name else "", " ", patient_opd.patient.last_name if patient_opd.patient.last_name else ""])
-    context["mobile_no"] = patient_opd.patient.phone
+    context["mobile_no"] = patient_opd.patient.mob_no_barcode
     context["address"] = "".join([" ", patient_opd.patient.city.city_name, " ",
                                   patient_opd.patient.district.district_name, " ",
                                   patient_opd.patient.taluka.taluka_name, " ", patient_opd.patient.state.state_name])
@@ -79,8 +87,6 @@ def consultation_rpt(request, id, language_id=None):
     context["pa"] = patient_opd.consultationmodel.pa_value
     context["ps"] = patient_opd.consultationmodel.ps
     context["pv"] = patient_opd.consultationmodel.pv
-
-    print(context)
 
     template_name = "reports/en/consultation.html"
     return render(request, template_name,
