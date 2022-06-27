42e17036f380a021264fe95d02aa6aff0b822c26
Quartz <akhatri@devopsinternational.nl>
2022-06-27 10:37:31 +0530

    report

--- reports/rpt/discharge_rpt.py
+++ reports/rpt/discharge_rpt.py
@@ -29,12 +29,16 @@ def discharge_rpt(request, id, language_id=None):
 
     context = {}
     context["regd_no"] = patient_opd.patient.regd_no_barcode
-    context["name"] = "".join(
-        [patient_opd.patient.first_name, " ", patient_opd.patient.middle_name, " ", patient_opd.patient.last_name])
-    context["address"] = "".join([" ", patient_opd.patient.city.city_name, " ",
-                                  patient_opd.patient.district.district_name, " ",
-                                  patient_opd.patient.taluka.taluka_name, " ", patient_opd.patient.state.state_name])
-
+    try:
+        context["name"] = "".join(
+            [patient_opd.patient.first_name, " ", patient_opd.patient.middle_name, " ", patient_opd.patient.last_name])
+        context["address"] = "".join([" ", patient_opd.patient.city.city_name, " ",
+                                    patient_opd.patient.district.district_name, " ",
+                                    patient_opd.patient.taluka.taluka_name, " ", patient_opd.patient.state.state_name])
+    except:
+        context["name"] = ""
+        context["address"] = ""
+        
     patient_discharge = PatientDischargeModel.objects.filter(patient_opd=patient_opd).first()
     if patient_discharge == None:
         context = {}
