2559f98aa665406d4e45990b5d5b7aff478048f8
Mital Tank <dhruv@devopsinternational.nl>
2022-07-21 17:52:34 +0530

    status field change

--- patient_opd/models.py
+++ patient_opd/models.py
@@ -17,7 +17,7 @@ class PatientOpdModel(models.Model):
     patient = models.ForeignKey(PatientModel, on_delete=models.DO_NOTHING)
     is_paid = models.BooleanField(default=False)
     status = models.CharField(
-        max_length=25, null=True, default="consultation"
+        max_length=25, null=True, default="opd"
     )  # This will be enable when any examination table gets entry of particular patient
     patient_type = models.CharField(
         max_length=5, choices=patient_type_choice, default="OB", null=True
