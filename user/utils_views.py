d6c430e3016815e4ccefdef494b4212e3e0aa44a
akhatri84 <akhatri@devopsinternational.nl>
2022-08-24 18:25:21 +0530

    create auto permission

--- user/utils_views.py
+++ user/utils_views.py
@@ -4,10 +4,9 @@ from django.contrib.auth.models import Group
 
 def create_profile(user):
     if user.user_type in ["HOSPITAL", "DOCTOR"]:
-        ObgynConfigModel.objects.create(
-            user=user
-        )
+        ObgynConfigModel.objects.create(user=user)
 
     group = Group.objects.filter(name__iexact=user.user_type).first()
     if group:
         group.user_set.add(user)
+        group.save()
